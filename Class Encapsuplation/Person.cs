921f659cd92077059beab85c510b62290db1411b
MahammadMammadov1 <bd6mhrhq0@code.edu.az>
2023-10-10 11:17:49 -0700

    all task done

---
+++ Class Encapsuplation/Person.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Class_Encapsuplation
+{
+    internal class Person
+    {
+		private int _phoneNumber;
+        private string _fullName;
+        private byte _age;
+
+
+
+        public Person(string fullName,int phoneNumber,byte age)
+        {
+            this._fullName = fullName;
+			this._phoneNumber = phoneNumber;
+			this._age = age;
+        }
+
+        public int PhoneNumber
+		{
+			get 
+			{
+				return _phoneNumber; 
+			}
+			set 
+			{ 
+				if (value > 0)
+				{
+                    _phoneNumber = value;
+                }
+				
+			}
+		}
+
+		
+
+		public string FullName
+		{
+			get 
+			{
+				return _fullName;
+            }
+            set 
+			{
+                string[] nameParts = value.Trim().Split(' '); // INTERNETDEN KOMEK ALDIM 
+
+                if (nameParts.Length >= 2 && !string.IsNullOrWhiteSpace(nameParts[1])) 
+                {
+                    _fullName = value;
+                }
+            
+            }
+		}
+
+		
+
+		public byte Age
+		{
+			get 
+			{ 
+				return _age;
+			}
+			set
+			{ 
+				if(value > 0 && value < 180) 
+				{ 
+					_age = value;
+				}
+				 
+			}
+		}
+
+
+	}
+}
---
+++ Class Encapsuplation/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Class_Encapsuplation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("Mehemmed Memmedov",122323434,19);
+            
+            Console.WriteLine(person1.FullName + "\n"+ person1.PhoneNumber + "\n"+ person1.Age);
+
+            Upper();
+
+
+
+            //TASK 2 
+
+            static void Upper()
+            {
+                 string name1 = Console.ReadLine();
+
+                if (name1.Length>0 && name1 != null)
+                {
+                    char first = name1[0];
+                    if (first >= 'a' && first <= 'z')
+                    {
+                        first = (char)(first - 32);
+                        Console.WriteLine("Ilk herfi boyuk olan soz : " + first + name1[1..]);
+                    }
+                    else Console.WriteLine("Ilk herfe reqem ve ya boyuk herf daxil edilib ");
+
+
+                }
+
+                else Console.WriteLine("duzgun metn daxil edin");
+            }
+        }
+    }
+}
